61d83d63f829aed1055ade1640856cd3cd993725
amine775 <amine_77500@hotmail.fr>
2019-12-13 07:17:29 +0100

    Creating class service to use API

--- app/src/main/java/com/example/mazappli/controller/WeatherService.kt
+++ app/src/main/java/com/example/mazappli/controller/WeatherService.kt
@@ -8,5 +8,7 @@ import retrofit2.http.Query
 
 interface WeatherService {
     @GET("data/2.5/weather?")
-    fun getCurrentWeatherData(@Query("lat") lat: String, @Query("lon") lon: String, @Query("APPID") app_id: String): Call<WeatherResponse>
+    // fun getCurrentWeatherData(@Query("lat") lat: String, @Query("lon") lon: String, @Query("APPID") app_id: String): Call<WeatherResponse>
+    fun getCurrentWeatherData(@Query("q") lon: String, @Query("APPID") app_id: String): Call<WeatherResponse>
+
 }
---
+++ app/src/main/java/com/example/mazappli/schemaclass/WeatherClass.kt
@@ -0,0 +1,9 @@
+package com.example.mazappli.schemaclass
+
+class WeatherClass {
+    var ville: String? = null
+    var pays: String?=null
+    var temp: String? =null
+    var temp_min: String? = null
+    var temp_max: String? = null
+}
